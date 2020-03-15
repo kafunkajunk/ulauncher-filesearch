eae04475c102860931a190fbe7542a968babf4e5
chris <kafunkajunk@gmail.com>
2020-03-14 21:32:20 -0600

    added gear image, readme docs

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
-from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
+from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
+from ulauncher.api.shared.action.OpenAction import OpenAction
 
 logger = logging.getLogger(__name__)
 
@@ -28,11 +30,15 @@ class KeywordQueryEventListener(EventListener):
         items = []
         logger.info('preferences %s' % json.dumps(extension.preferences))
         db_path = extension.preferences['fi_path']
-        
-        logger.info('query %s' % event.get_argument())
-        result = subprocess.run(["mlocate", "-i", "-b", "-l", "5", "-d", "/home/kafunkajunk/home.db", event.get_argument()], capture_output=True, text=True)
+
+        query = event.get_argument()
+        if query == None:
+            return 
+
+        logger.info('query %s' % query)
+        logger.info('db_path %s' % db_path)
+        result = subprocess.run(["mlocate", "-i", "-b", "-l", "5", "-d", "/home/kafunkajunk/home.db", query], capture_output=True, text=True)
         result = result.stdout.splitlines()
-        logger.info('result: %s' % result)
         range_len = len(result) if len(result) <= 5 else 5
 
         for index in range(range_len):
@@ -44,40 +50,38 @@ class KeywordQueryEventListener(EventListener):
             title = os.path.basename(os.path.normpath(item)) if os.path.isdir(item) else os.path.basename(item)
 
             data = {'path': item}
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                             name=title,
-                                             description=item,
-                                             on_enter=ExtensionCustomAction(data, keep_app_open=True)))
-            
-            #item_name = extension.preferences['item_name']
-            #data = {'new_name': '%s %s was clicked' % (item_name, i)}
-            #items.append(ExtensionResultItem(icon='images/icon.png',
-            #                                 name='%s %s' % (item_name, i),
-            #                                 description='Tits:  %s' % result[i],
-            #                                 on_enter=ExtensionCustomAction(data, keep_app_open=True)))
+            items.append(ExtensionResultItem(icon='images/papirus-gear-128.png',
+                name=title,
+                description=item,
+                on_enter=ExtensionCustomAction(data, keep_app_open=True)))
 
         return RenderResultListAction(items)
 
 
 class ItemEnterEventListener(EventListener):
 
-    def build_actions(path):
+    def __init__(self):
+        super(ItemEnterEventListener, self).__init__()
 
-        yield ['echo', path, 'xclip', '-sel', 'clip']
-        yield ['thunar', path]
 
-    def execute_action(action_args):
-        subprocess.run(action_args)
-        HideWindowAction()
-        
-        
+    def build_actions(self, path):
+        yield ('Open', OpenAction(path))
+        yield ('Copy Path', CopyToClipboardAction(path))
+
+
     def on_event(self, event, extension):
 
         data = event.get_data()
-        results = [ for x in build_actions(data['path']:
-            ExtensionResultItem(icon='images/icon.png', name=data['path'], on_enter=execute_action(x)) ]
-        
+        path = data['path']
+
+        results = []
+        for item in self.build_actions(path):
+
+            (verb, action) = item
+            results.append(ExtensionResultItem(icon='images/papirus-gear-128.png', name='{verb} {path}'.format(verb=verb, path=path), on_enter=action) )
+
         return RenderResultListAction(results)
+
 
 
 if __name__ == '__main__':
---
+++ readme.md
@@ -0,0 +1,24 @@
+# README
+--------
+
+# Summary
+
+This project is a python plugin for [https://ulauncher.io/](ulauncher).
+
+It provides similar file searching functionality to Alfred and Spotlight by utilizing `updatedb` and `mlocate`.
+
+If a result is found, the plugin then prompts for actions such as:
+
+   - opening the file.
+   - copying the full path to the clipboard.
+
+Potentially, any action exposed through the plugin api could be added. 
+
+# Dependencies
+
+This plugin needs a path to a db file which contains any relevant indexed files on the system. 
+
+# Notices
+
+Icon is from papirus icon set [https://github.com/PapirusDevelopmentTeam/papirus-icon-theme](here)
+
